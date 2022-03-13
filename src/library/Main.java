fa41f9205f131fd7e80eaf1f466e5e549ae5f27b
JigneshSatam <jigneshsatam@gmail.com>
2022-03-13 16:00:29 -0400

    Added database connection comments and main class comments

--- src/library/Main.java
+++ src/library/Main.java
@@ -1,6 +1,7 @@
 package library;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -16,8 +17,15 @@ public class Main extends Application {
   private ItemController itemCtrl;
   private Connection connection;
 
+
+  /**
+   * start method is invoked to start JavaFX application
+   *
+   * @param stage is a stage for JavaFX application
+   * @throws SQLException if any execption occurs while accessing the database
+   */
   @Override
-  public void start(Stage stage) throws Exception {
+  public void start(Stage stage) throws SQLException {
     connection = MySqlConnection.getConnection();
     VBox vbox = new VBox();
     itemCtrl = new ItemController(connection, vbox);
@@ -34,13 +42,25 @@ public class Main extends Application {
     ItemForm.setStatus(vbox, ItemForm.ADD_BOOK);
   }
 
+
+  /**
+   * Stop is invoked when the JavaFX application is closed.
+   * It closes the database connection if any
+   *
+   * @throws SQLException
+   */
   @Override
-  public void stop() throws Exception {
+  public void stop() throws SQLException {
     if (connection != null) {
       connection.close();
     }
   }
 
+
+  /**
+   * main is the entry point of the application
+   * @param args
+   */
   public static void main(String[] args) {
     launch(args);
   }
--- src/library/databaseConnections/MySqlConnection.java
+++ src/library/databaseConnections/MySqlConnection.java
@@ -5,14 +5,30 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySqlConnection {
-
+  /**
+   * Mysql database url with name of the {@code databse} to connect
+   */
   private static final String dbURL =  "jdbc:mysql://localhost:3306/Library";
+  /**
+   * Mysql database user
+   */
   private static final String user = "root";
+  /**
+   * Mysql database password
+   */
   private static final String password = "root";
+
   private static Connection connection;
 
   private MySqlConnection() {}
 
+
+  /**
+   * getConnection returns {@code mysql} connection or throws {@code SQLException}
+   *
+   * @return Connection to mysql database
+   * @throws SQLException if a database access error occurs or the url is {@code null}
+   */
   public static Connection getConnection() throws SQLException {
     if (connection == null) {
       synchronized (MySqlConnection.class) {
@@ -24,6 +40,11 @@ public class MySqlConnection {
     return connection;
   }
 
+
+  /**
+   * Closes the connection to {@code mysql} database
+   * @throws SQLException if error is occured while closing the connection
+   */
   public void close() throws SQLException {
     if (connection != null) {
       connection.close();
